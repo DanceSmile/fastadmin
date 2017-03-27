7f7abeeedf065a653f170b82c692b5302df70b08
cailei <cailei@aprbrother.com>
2017-03-27 16:54:55 +0800

    provider

--- src/Providers/FastAdminServiceProvider.php
+++ src/Providers/FastAdminServiceProvider.php
@@ -14,8 +14,25 @@ class FastAdminServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'../config/admin.php' => config_path('admin.php'),
-        ]);
+            __DIR__.'/../config/admin.php' => config_path('admin.php')
+        ], 'config');
+
+        $this->publishes([
+            __DIR__.'/../resource/fast-admin' => public_path('fast-admin'),
+            __DIR__.'/../resource/components' => resource_path('fast-admin'),
+            __DIR__.'/../resource/layouts' => resource_path('fast-admin'),
+            __DIR__.'/../resource/admin' => resource_path('fast-admin')
+        ], 'resource');
+
+        $this->publishes([
+//            __DIR__.'/../database/migrations/' => database_path('migrations')
+        ], 'migrations');
+
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+
+
+
     }
 
     /**
---
+++ src/route/routes.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siyue
+ * Date: 2017/3/27
+ * Time: 16:32
+ */
+
+Route::get("admin",function(){
+    return view("admin.index");
+});
